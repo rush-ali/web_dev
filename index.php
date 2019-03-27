48ca884ce768a2f5c338ea980f8be84514dbc82a
Rushali Gupta <soni.rushali@gmail.com>
2019-03-27 23:28:56 +0530

    files related to web dev

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width"> <!according to the device's size>
+	<meta name="description" content="affortable and professional web design">
+	<meta name="keywords" content="web design, affortable web services, professional web services">
+	<meta name="author" content="rushali gupta">
+	<title> bestair web design | welcome </title>
+	<link rel="stylesheet" href="style.css">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+
+<body>
+	<header>
+		<div class="container">
+			<div id="branding">
+				<h1><spam class="highlight">Bestair Events</spam></h1>	
+			</div>
+			<!--<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<!Brand and toggle get grouped for better mobile display 
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">HOME</a>
+					</div>
+
+					 Collect the nav links, forms, and other content for toggling -
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+							<li><a href="about.html">ABOUT</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+									<li><a href="#">Something else here</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Separated link</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">One more separated link</a></li>
+								</ul>
+							</li>
+						</ul>
+						<form class="navbar-form navbar-left">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Search">
+							</div>
+							<button type="submit" class="btn btn-default">Submit</button>
+						</form>
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Link</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+									<li><a href="#">Something else here</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Separated link</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>< /.navbar-collapse 
+				</div> /.container-fluid 
+			</nav>-->
+			<nav><!--for menu>-->
+				<ul><!--mostly used for menus>-->
+					<li class="current"><a href="index.php">Home</a></li>
+					<li><a href="about.html">About us</a></l>
+					<li><a href="services.html">Hotel</a></li>
+					<li><a href="clients.html">Clients</a></li>
+					<li><a href="contact us.html">Contact us</a></li>
+				</ul>
+
+				</nav>
+			</div>
+		</header>
+
+		<section id="showcase">
+			<div class="container">
+				<h1> Affordable Professional Services</h1>
+				<p>Bestair Services is one of the fastest emerging entity in the domain of reservation services for their preferred clients for Corporate Events, Travel, Hotel Bookings, Special arrangements, meetings, Exhibitions and other Travel related services. We are considered to be a reliable company for customized reservation services helpdesk in the field of hospitality for corporate clients.</p>
+				<p>	
+				We are a single window solution provider for clients when it comes to client satisfaction & service management. The vast experience of our management team has enabled us to partner with the best hotels and other hospitality partners. We have promoting this company for our clients in almost every part of the country.</p>
+
+			</div>
+		</section>
+		<section id="newsletter">
+			<div class="container">
+				<h1>Subscribe to our newsletter</h1>
+				<form>
+					<input type="email" placeholder="enter email...">
+					<button type="submit" class="button_1">subscribe</button>
+				</form>
+			</div>
+		</section>
+
+		<section id="boxes">
+			<div class="container">
+				<div class="box">
+					<img src="images/conf.png">
+					<h2>conferences</h2>
+					<p>provides private conferences</p>
+				</div>
+				<div class="box">
+					<img src="images/vip.png">
+					<h2>private parties</h2>
+					<p>provides private spacious parties</p>
+				</div>
+				<div class="box">
+					<img src="images/event.png">
+					<h2>coorperate events</h2>
+					<p>providesany coorporate events according to the specifications</p>
+				</div>
+				<div class="box">
+					<img src="images/cultural.png">
+					<h2>cultural events</h2>
+					<p>provides promotion of cultural events</p>
+				</div>
+				<div class="box">
+					<img src="images/pavilions.png">
+					<h2>exibition pavilions</h2>
+					<p>provides exibition spacious and meaningful</p>
+				</div>
+				<div class="box">
+					<img src="images/rise.png">
+					<h2>corporate promotion</h2>
+					<p>provides space to promote your company</p>
+				</div>
+			</div>
+		</section>
+		<footer>
+			<p> Bestair Events, Copyright &copy; <?php echo date('Y')?></p>
+		</footer>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> -->
+	</body>
+	</html>
